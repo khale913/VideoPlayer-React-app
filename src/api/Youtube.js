3746d8eb31cf4a9ad654f57fcb91af6861a8da3e
Kelton Hale <keltonhale33@gmail.com>
2021-11-11 09:44:03 -0700

    added seperate component for API request

---
+++ src/api/Youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "AIzaSyDZA_VCIz3NuAtQLsO3BQg567RfqthIli8";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
