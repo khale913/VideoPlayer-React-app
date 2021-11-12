5ee35aaa9f79271d2ec49d121f904d19d9c31e80
Kelton Hale <keltonhale33@gmail.com>
2021-11-12 10:13:23 -0700

    loaded video upon click

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -3,9 +3,21 @@ import React from "react";
 const VideoDetail = (props) => {
   if (!props.video) {
     return <div>Loading...</div>;
-  } else {
-    return <div>{props.video.snippet.title}</div>;
   }
+
+  const videoSrc = `https://www.youtube.com/embed/${props.video.id.videoId}`;
+
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe title={props.video.snippet.title} src={videoSrc} />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{props.video.snippet.title}</h4>
+        <p>{props.video.snippet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
